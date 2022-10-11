b85e86ec563224ad1efda7edfe12e87e7e5b0b26
Max140501 <113982178+Max140501@users.noreply.github.com>
2022-10-11 11:37:56 +0200

    Update prix.js

--- prix.js
+++ prix.js
@@ -1,11 +1,13 @@
-
-Let Taillet=[5,10,20]
-Let Dureet=[7,14,30,60,365]
-Let Expositiont=[2,1,0]
-Let Eclairagedenuitt=[2,1,0]
-Let Assurancet=[1,0]
-Let Livraisont=[25,0]
-
-const Prix = (Taillet[Taille.options[Taille.selectedIndex].id] + Expositiont[Exposition.options[Exposition.selectedIndex].id] + Eclairagedenuitt[Eclairagedenuit.options[Eclairagedenuit.selectedIndex].id] + Assurancet[Assurance.options[Assurance.selectedIndex].id]) * Dureet[Duree.options[Duree.selectedIndex].id] + Livraisont[Livraison.options[Livraison.selectedIndex].id];
-
-(Taillet[0] + Expositiont[2] + Eclairagedenuitt[0] + Assurancet[0]) * Dureet[0] + Livraisont[0];
+<script type="text/javascript">
+function calculTotal(){
+    var Taille = Number(panier.getElementById("Taille").value);
+    var Duree = Number(panier.getElementById("Duree").value);
+    var Exposition = Number(panier.getElementById("Exposition").value);
+    var Eclairagedenuit = Number(panier.getElementById("Eclairagedenuit").value);
+    var Assurance = Number(panier.getElementById("Assurance").value);
+    var Livraison = Number(panier.getElementById("Livraison").value);
+  
+    var amount = Number(((Taille + Exposition + Eclairagedenuit + Assurance) * Duree) + Livraison) ;
+    panier.getElementById("amount") = amount;
+}
+</script>
