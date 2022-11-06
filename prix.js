d2d8edc74fa03de0da07df39218b1621a1cb2448
Max140501 <113982178+Max140501@users.noreply.github.com>
2022-11-06 08:35:11 +0100

    Update prix.js

--- prix.js
+++ prix.js
@@ -1,33 +1,41 @@
-
-function getid(selName){ //fonction qui permet de changer l'image suivant le formulaire
+function Prix(selName){ //fonction calculant le prix d'un produit
+    localStorage.clear();
     select = document.getElementById(selName);
-    id=(select.options[select.selectedIndex].text);
-    document.getElementById(id).innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">';
-}
-   
-function calculTotal(){ //fonction calculant le prix d'un produit
-    select = document.getElementByValue(selName);
-    value=select.options[select.selectedIndex].text;
-    document.prix.innerHTML = value
+    valueTaille = (Taille.options[Taille.selectedIndex].value)
+    valueAssurance=(Assurance.options[Assurance.selectedIndex].value);
+    valueEclairagedenuit=(Eclairagedenuit.options[Eclairagedenuit.selectedIndex].value);
+    prix = Taille.options[Taille.selectedIndex].value + Eclairagedenuit.options[Eclairagedenuit.selectedIndex].value + Assurance.options[Assurance.selectedIndex].value;
+    document.getElementById("prix").innerHTML=prix
+    idT=(Taille.options[Taille.selectedIndex].text);
+    idA=(Assurance.options[Assurance.selectedIndex].text);
+    idC=(Couleurs.options[Couleurs.selectedIndex].text);
+    idE=(Eclairagedenuit.options[Eclairagedenuit.selectedIndex].text);
+    if(idT=="100x200"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
+    if(idT=="200x400"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
+    if(idT=="400x800"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
+    if(idT=="100x200" && idE=="Eclairage" ){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
+    if(idT=="200x400" && idE=="Eclairage"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
+    if(idT=="400x800" && idE=="Eclairage"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
+    if(idT=="100x200" && idE=="Rien"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
+    if(idT=="200x400" && idE=="Rien"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
+    if(idT=="400x800" && idE=="Rien"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
+    localStorage.setItem("idT",idT);
+    localStorage.setItem("idE",idE);
+    localStorage.setItem("idA",idA);
+    localStorage.setItem("idC",idC);
+    localStorage.setItem("Image",document.getElementById("image"));
+    localStorage.setItem("Prix",Prix);
 }
 
 //let btnPlus = document.querySelector() //fonction qui permet d'augmenter le nombre de produit achete
 
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWF4MTQwNTAxIiwiYSI6ImNsYTNzMmlobjA1MXAzdm14dngyeG1zazQifQ.2IHWppCoE_B-epIl553RXg';
-//const map = new mapboxgl.Map({
-    //container: 'map',
-    //style: 'mapbox://styles/mapbox/streets-v11',
-    //center: [-122.662323, 45.523751], // starting position
-    //zoom: 12
-//    });
-//const bounds = [
-//    [-123.069003, 45.395273],
-//    [-122.303707, 45.612333]
-//  ];
-//map.setMaxBounds(bounds);
+
 const start = [45.7627925, 4.8630073];
-const end = [46,5]
+x=input()
+y=input()
+const end = [x,y];
 async function getRoute(end) {
     const query = await fetch(
 `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
@@ -35,5 +43,5 @@ async function getRoute(end) {
     );
     const json = await query.json();
     const data = json.routes[0];
-    console.error(data)
+    console.log(data)
 }
