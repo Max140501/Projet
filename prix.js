66674b85a0a9f0800b7cf35837021ca54ca05004
Max140501 <113982178+Max140501@users.noreply.github.com>
2022-11-06 10:39:31 +0100

    Update prix.js

--- prix.js
+++ prix.js
@@ -1,47 +1,93 @@
-function Prix(selName){ //fonction calculant le prix d'un produit
+function Prix(selName){ //fonction calculant le prix d'un produit et la personnalisation
     localStorage.clear();
     select = document.getElementById(selName);
     valueTaille = (Taille.options[Taille.selectedIndex].value)
     valueAssurance=(Assurance.options[Assurance.selectedIndex].value);
     valueEclairagedenuit=(Eclairagedenuit.options[Eclairagedenuit.selectedIndex].value);
-    prix = Taille.options[Taille.selectedIndex].value + Eclairagedenuit.options[Eclairagedenuit.selectedIndex].value + Assurance.options[Assurance.selectedIndex].value;
-    document.getElementById("prix").innerHTML=prix
-    idT=(Taille.options[Taille.selectedIndex].text);
+    idT=(Taille.options[Taille.selectedIndex].text);//variable pour le local storage
     idA=(Assurance.options[Assurance.selectedIndex].text);
     idC=(Couleurs.options[Couleurs.selectedIndex].text);
     idE=(Eclairagedenuit.options[Eclairagedenuit.selectedIndex].text);
-    if(idT=="100x200"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
-    if(idT=="200x400"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
-    if(idT=="400x800"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
-    if(idT=="100x200" && idE=="Eclairage" ){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
-    if(idT=="200x400" && idE=="Eclairage"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
-    if(idT=="400x800" && idE=="Eclairage"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
-    if(idT=="100x200" && idE=="Rien"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
-    if(idT=="200x400" && idE=="Rien"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
-    if(idT=="400x800" && idE=="Rien"){document.getElementById("image").innerHTML = '<img src="C:/Users/maxgi/Desktop/site/i2.jpg">'}
-    localStorage.setItem("idT",idT);
+    
+    if(idT=="100x200"){document.getElementById("image").innerHTML = '<img src="panneaux1.png">',
+    prix=50,
+    document.getElementById("prix").innerHTML=prix
+    Image= '<img src="panneaux1.png">'
+    }
+
+    if(idT=="200x400"){document.getElementById("image").innerHTML = '<img src="panneaux2.png">',
+    prix=100,
+    document.getElementById("prix").innerHTML=prix
+    Image= '<img src="panneaux2.png">'}
+
+    if(idT=="400x800"){document.getElementById("image").innerHTML = '<img src="panneaux3.png">',
+    prix=200,
+    document.getElementById("prix").innerHTML=prix
+    Image= '<img src="panneaux3.png">'}
+
+    if(idT=="100x200" && idC=="Bleu" ){document.getElementById("image").innerHTML = '<img src="panneaux1bleu.png">',
+    prix=55,
+    document.getElementById("prix").innerHTML=prix
+    Image= '<img src="panneaux1bleu.png">'}
+
+    if(idT=="100x200" && idC=="Rouge"){document.getElementById("image").innerHTML = '<img src="panneaux1rouge.png">',
+    prix=55,
+    document.getElementById("prix").innerHTML=prix
+    Image= '<img src="panneaux1rouge.png">'}
+
+    if(idT=="100x200" && idC=="Vert"){document.getElementById("image").innerHTML = '<img src="panneaux1vert.png">',
+    prix=55,
+    document.getElementById("prix").innerHTML=prix
+    Image= '<img src="panneaux1vert.png">'}
+
+    if(idT=="200x400" && idC=="Bleu"){document.getElementById("image").innerHTML = '<img src="panneaux2bleu.png">',
+    prix=105,
+    document.getElementById("prix").innerHTML=prix
+    Image= '<img src="panneaux2bleu.png">'}
+
+    if(idT=="200x400" && idC=="Rouge"){document.getElementById("image").innerHTML = '<img src="panneaux2rouge.png">',
+    prix=105,
+    document.getElementById("prix").innerHTML=prix
+    Image= '<img src="panneaux2rouge.png">'}
+
+    if(idT=="200x400" && idC=="Vert"){document.getElementById("image").innerHTML = '<img src="panneaux2vert.png">',
+    prix=105,
+    document.getElementById("prix").innerHTML=prix
+    Image= '<img src="panneaux2vert.png">'}
+
+    if(idT=="400x800" && idC=="Bleu"){document.getElementById("image").innerHTML = '<img src="panneaux3bleu.png">',
+    prix=205,
+    document.getElementById("prix").innerHTML=prix
+    Image= '<img src="panneaux3bleu.png">'}
+
+    if(idT=="400x800" && idC=="Rouge"){document.getElementById("image").innerHTML = '<img src="panneaux3rouge.png">',
+    prix=205,
+    document.getElementById("prix").innerHTML=prix
+    Image= '<img src="panneaux3rouge.png">'}
+
+    if(idT=="400x800" && idC=="Vert"){document.getElementById("image").innerHTML = '<img src="panneaux3vert.png">',
+    prix=205,
+    document.getElementById("prix").innerHTML=prix
+    Image= '<img src="panneaux3vert.png">'}
+
+    localStorage.setItem("idT",idT); //local storage
     localStorage.setItem("idE",idE);
     localStorage.setItem("idA",idA);
     localStorage.setItem("idC",idC);
-    localStorage.setItem("Image",document.getElementById("image"));
-    localStorage.setItem("Prix",Prix);
+    localStorage.setItem("Image",Image);
+    localStorage.setItem("prix",prix);
 }
-
-//let btnPlus = document.querySelector() //fonction qui permet d'augmenter le nombre de produit achete
-
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWF4MTQwNTAxIiwiYSI6ImNsYTNzMmlobjA1MXAzdm14dngyeG1zazQifQ.2IHWppCoE_B-epIl553RXg';
 
 const start = [45.7627925, 4.8630073];
-x=input()
-y=input()
-const end = [x,y];
+const end = [input(x),input(y)]
 async function getRoute(end) {
     const query = await fetch(
-`https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+`https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${x},${y}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
     { method: 'GET' }
     );
     const json = await query.json();
     const data = json.routes[0];
-    console.log(data)
+    console.error(data)
 }
