4b852cdb3517ae9836f4a182c97387d220190c08
salsinats1 <113982334+salsinats1@users.noreply.github.com>
2022-11-06 20:54:06 +0100

    Update site.js

--- site.js
+++ site.js
@@ -91,3 +91,30 @@ async function getRoute(end) {
     const data = json.routes[0];
     console.error(data)
 }
+//code relié au diaporama de la page d'accueil
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("custom-slider");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+} 
